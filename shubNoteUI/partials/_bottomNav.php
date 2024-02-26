571797ef382ce04ff2d0e95c6f2cdc6cee69e8ae
shubham-amlani <shubhamamlani104@gmail.com>
2024-02-26 22:58:29 +0530

    Completed coding new UI of shubNOte

---
+++ shubNoteUI/partials/_bottomNav.php
@@ -0,0 +1,19 @@
+<div class="bottom-nav d-flex justify-content-center fixed-bottom align-items-center">
+    <ul class="nav justify-content-evenly align-items-center w-100 h-100">
+        <li class="nav-item">
+            <a class="nav-link active" href="home.php"><i class="fas fa-home"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="explore.php"><i class="fas fa-compass"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="search.php"><i class="fas fa-search"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about.php"><i class="fas fa-info-circle"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="myaccount.php"><i class="fas fa-user"></i></a>
+        </li>
+    </ul>
+</div>
---
+++ shubNoteUI/partials/_footer.php
@@ -0,0 +1,14 @@
+<?php
+echo '
+<footer class="footer bg-dark py-3 fixed-bottom">
+<div class="container text-center text-light p-0">
+    <div class="row justify-content-center ">
+        <div class="col-md-6">
+            <p class="mb-0">Designed and developed by Shubham Amlani</p>
+            <p class="mb-0">All rights reserved &copy; 2024</p>
+        </div>
+    </div>
+</div>
+</footer>
+';
+?>
